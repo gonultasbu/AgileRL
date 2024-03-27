b21da76be443f5d54f5ccd141f2a518ff8448aef
gonultasbu <gonultasbu@users.noreply.github.com>
2024-03-27 16:44:07 -0500

    Revert "FOR TESTING ZERO CNN OUTPUT"
    This reverts commit b93aca2a15353cd9f5bc5a236815d051ad7c5efc.
    
    # Conflicts:
    #	agilerl/networks/evolvable_cnn.py

--- agilerl/networks/evolvable_cnn.py
+++ agilerl/networks/evolvable_cnn.py
@@ -506,9 +506,7 @@ class EvolvableCNN(nn.Module):
 
         if self.normalize:
             x = x / 255.0
-        # if self.mixed_input:
-        #     x = torch.zeros(([batch_size, 4]), device=self.device, dtype=torch.float32)
-        # else:
+
         x = self.feature_net(x)
         x = x.reshape(batch_size, -1)
         if self.critic or self.mixed_input:
